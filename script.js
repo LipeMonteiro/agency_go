dd4c8ac03403f62cb89fe7caaf8bbdc859d19dcd
Filipe Monteiro <filipealexandre01@hotmail.com>
2025-05-26 13:48:29 -0300

    Style: Finalização da Home page e about page iniciada com a adição das estatísticas

---
+++ script.js
@@ -0,0 +1,5 @@
+// ========== HEADER STICKY SFFECT ==========
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('#header')
+    header.classList.toggle('sticky', window.scrollY > 0)
+})
