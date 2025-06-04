a86823ad0c4874e8868112af9a2ea64752b2b0e4
LipeMonteiro <filipealexandre01@hotmail.com>
2025-06-04 07:52:44 -0300

    Feat: Active link on scroll animation add

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 // ========== HEADER STICKY SFFECT ==========
 window.addEventListener('scroll', () => {
-    const header = document.querySelector('#header')
-    header.classList.toggle('sticky', window.scrollY > 0)
+  const header = document.querySelector('#header')
+  header.classList.toggle('sticky', window.scrollY > 0)
+})
+// ========== ACTIVE LINK ON SCROLL ANIMATION ==========
+
+const headerLinks = document.querySelectorAll('.header__menu-link')
+const sections = document.querySelectorAll('section')
+
+window.addEventListener('scroll', () => {
+
+
+  headerLinks.forEach(link => {link.classList.remove('active')})
+
+  sections.forEach(element => {
+    let top = window.scrollY
+    let offset = element.offsetTop - 150
+    let height = element.offsetHeight
+    const id = element.getAttribute('id')
+
+    if (top >= offset && top < offset + height) {
+      document.querySelector(`header div nav a[href='#${id}']`).classList.add('active')
+    }
+    
+  });
 })
 
-// Função que anima a contagem
+// ========== STATICS ANIMATION ==============
 function animarContador(elemento, duracao = 2000) {
   const valorFinal = +elemento.getAttribute('data-valor');
   const incremento = Math.ceil(valorFinal / (duracao / 32)); // 60fps ~16ms por frame
